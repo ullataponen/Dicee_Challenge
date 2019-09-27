5d85ead713ef96143fb9e16e5f8a3aadb5802bc0
ullataponen <ulla.taponen@gmail.com>
2019-09-27 22:39:12 +0200

    Improve js readability, change bg color

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,13 +1,12 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-document
-  .querySelector(".img1")
-  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+let randomImage = "images/dice" + randomNumber1 + ".png";
+let randomImage2 = "images/dice" + randomNumber2 + ".png";
 
-document
-  .querySelector(".img2")
-  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+document.querySelector(".img1").setAttribute("src", randomImage);
+
+document.querySelector(".img2").setAttribute("src", randomImage2);
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").textContent = "👉 Player 1 wins!";
